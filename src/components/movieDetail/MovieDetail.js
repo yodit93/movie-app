1da16ebd7aa701b8c8b1094886a3c2068ed2050e
yodit <yoditabebe9391gmail.com>
2023-04-27 09:50:29 +0300

    Render selected movie/show using useParams to get id from url

--- src/components/movieDetail/MovieDetail.js
+++ src/components/movieDetail/MovieDetail.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAsyncMovieOrShow } from '../../features/movies/movieSlice';
+import { fetchAsyncMovieOrShow, removeSelectedMovieOrShow } from '../../features/movies/movieSlice';
 import './MovieDetail.scss';
 
 const MovieDetail = () => {
@@ -10,68 +10,77 @@ const MovieDetail = () => {
   const movie = useSelector((state) => state.selectedMovieOrShow);
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShow(imdbID));
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
   }, [dispatch, imdbID]);
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <h2 className="movie-title">{movie.Title}</h2>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating
-            <i className="fa fa-star" />
-            :
-            {' '}
-            {movie.imdbRating}
-          </span>
-          <span>
-            IMDB Votes
-            <i className="fa fa-thumbs-up" />
-            :
-            {' '}
-            {movie.imdbVotes}
-          </span>
-          <span>
-            Runtime
-            <i className="fa fa-film" />
-            :
-            {' '}
-            {movie.Runtime}
-          </span>
-          <span>
-            Year
-            <i className="fa fa-calendar" />
-            :
-            {' '}
-            {movie.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{movie.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{movie.Director}</span>
-          </div>
-          <div>
-            <span>Stars</span>
-            <span>{movie.Actors}</span>
-          </div>
-          <div>
-            <span>Generes</span>
-            <span>{movie.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{movie.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{movie.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={movie.Poster} alt={movie.Title} />
-      </div>
+      {Object.keys(movie).length === 0
+        ? (<div>Loading...</div>)
+        : (
+          <>
+            <div className="section-left">
+              <h2 className="movie-title">{movie.Title}</h2>
+              <div className="movie-rating">
+                <span>
+                  IMDB Rating
+                  <i className="fa fa-star" />
+                  :
+                  {' '}
+                  {movie.imdbRating}
+                </span>
+                <span>
+                  IMDB Votes
+                  <i className="fa fa-thumbs-up" />
+                  :
+                  {' '}
+                  {movie.imdbVotes}
+                </span>
+                <span>
+                  Runtime
+                  <i className="fa fa-film" />
+                  :
+                  {' '}
+                  {movie.Runtime}
+                </span>
+                <span>
+                  Year
+                  <i className="fa fa-calendar" />
+                  :
+                  {' '}
+                  {movie.Year}
+                </span>
+              </div>
+              <div className="movie-plot">{movie.Plot}</div>
+              <div className="movie-info">
+                <div>
+                  <span>Director</span>
+                  <span>{movie.Director}</span>
+                </div>
+                <div>
+                  <span>Stars</span>
+                  <span>{movie.Actors}</span>
+                </div>
+                <div>
+                  <span>Generes</span>
+                  <span>{movie.Genre}</span>
+                </div>
+                <div>
+                  <span>Languages</span>
+                  <span>{movie.Language}</span>
+                </div>
+                <div>
+                  <span>Awards</span>
+                  <span>{movie.Awards}</span>
+                </div>
+              </div>
+            </div>
+            <div className="section-right">
+              <img src={movie.Poster} alt={movie.Title} />
+            </div>
+          </>
+        )}
     </div>
   );
 };
